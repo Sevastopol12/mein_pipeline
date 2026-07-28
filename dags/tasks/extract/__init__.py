858272e18bc05641ab3dcf7993fdbc5afe649aa1
Sevastopol12 <chkalov253@gmail.com>
2026-07-28 20:50:07 +0700

    refactor: remove deprecated imports & simplify codebase

--- dags/tasks/extract/__init__.py
+++ dags/tasks/extract/__init__.py
@@ -1,64 +1,115 @@
 import logging
 
-from .connection import get_async_client
-from .utils import (
-    get_documents,
-    deduplicate,
-    upload_event_files,
-    extract_file_contents,
-    load_to_storage,
-    PoisonPill as stop_condition,
+from pydantic import BaseModel, ValidationError
+from typing import Optional
+
+from asyncio import Queue
+from google.genai.types import File, GenerateContentConfig, GenerateContentResponse
+from google.genai.errors import ClientError, ServerError
+
+from tenacity import (
+    AsyncRetrying,
+    wait_exponential_jitter,
+    retry_if_exception,
+    stop_after_attempt,
 )
 
-from asyncio import Queue, gather, create_task
-
-from airflow.sdk import task
-from dags.storage import EventItem as RESPONSE_SCHEMA
+from aiolimiter import AsyncLimiter
 
 
 logger = logging.getLogger(__name__)
 
 
-@task(task_id="extract")
-async def run_extract():
-    models = ["gemma-4-31b-it", "gemma-4-26b-a4b-it"]
-    documents = get_documents()
-
-    async with get_async_client() as client:
-        file_to_upload = await deduplicate(documents, client)
-        upload_results = await upload_event_files(file_to_upload, client)
-
-        task_queue = Queue()
-        # Assign task
-        for file in upload_results:
-            task_queue.put_nowait(file)
-
-        # Stop condition
-        for _ in range(len(models)):
-            task_queue.put_nowait(stop_condition())
-
-        instruction = "Extract content from the following file: "
-        tasks = [
-            create_task(
-                extract_file_contents(
-                    pending_queue=task_queue,
-                    client=client,
-                    model_name=model_name,
-                    instruction=instruction,
-                    response_schema=RESPONSE_SCHEMA,
-                )
-            )
-            for model_name in models
-        ]
-
-        results = await gather(*tasks)
-
-        logger.info("All task completed. Shutting down workers...")
-        for task in tasks:
-            task.cancel()
-        logger.info("All workers shutdown.")
-
-        load_to_storage(results)
+class PoisonPill:
+    def __init__(self):
+        pass
 
 
-__all__ = ["get_async_client", "run_extract"]
+async def extract_file_contents(
+    pending_queue: Queue[File | PoisonPill],
+    client,
+    model_name: str,
+    instruction: str,
+    response_schema: Optional[BaseModel],
+    rpm: int = 15,
+    max_attempts: int = 10,
+):
+    rpm_limit = AsyncLimiter(rpm)
+
+    resolved: list[bytes] = []
+    unresolved: list[File] = []
+
+    while True:
+        try:
+            task = await pending_queue.get()
+
+            if isinstance(task, PoisonPill):
+                logger.info("No task left. Returning...")
+                return {"resolved": resolved, "unresolved": unresolved}
+
+            logger.debug(f"{model_name} got file: {task.name}")
+            async for attempt in AsyncRetrying(
+                stop=stop_after_attempt(max_attempts),
+                retry=retry_if_exception(_retry),
+                wait=ResponseBaseWait(),
+            ):
+                with attempt:
+                    async with rpm_limit:
+                        json_response_object = await _get_content(
+                            task, client, model_name, instruction, response_schema
+                        )
+                    resolved.append(json_response_object)
+                    logger.info(f"[{task.name}] content extracted.")
+
+        except (ClientError, ServerError, ValidationError) as e:
+            logger.critical(e)
+            unresolved.append(task)
+
+        except Exception:
+            logger.error("Max attempt reached. Returning...")
+            unresolved.append(task)
+
+        finally:
+            pending_queue.task_done()
+
+
+async def _get_content(
+    file: File,
+    client,
+    model_name: str,
+    instruction: str,
+    response_schema: Optional[BaseModel],
+) -> bytes:
+    response: GenerateContentResponse = await client.models.generate_content(
+        model=model_name,
+        contents=[instruction, file],
+        config=GenerateContentConfig(
+            response_mime_type="application/json", response_schema=response_schema
+        ),
+    )
+    content = response.text.replace("*", "").replace("`", "").strip()
+
+    if not response_schema.model_validate_json(content):
+        raise ValidationError()
+
+    return content
+
+
+def _retry(exception) -> bool:
+    if isinstance(exception, ClientError) and exception.code != 429:
+        return False
+    return True
+
+
+class ResponseBaseWait:
+    def __init__(self):
+        self.default_wait = wait_exponential_jitter(initial=2, max=60)
+
+    def __call__(self, retry_state):
+        exception = retry_state.outcome.exception()
+
+        if exception.code == 429:
+            return 60
+        if exception.code == 503:
+            return 4
+        return self.default_wait(retry_state=retry_state)
