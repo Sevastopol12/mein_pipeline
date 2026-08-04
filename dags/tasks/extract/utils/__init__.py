4756a92933466a76c6293ca7e966d2aa556aea86
Sevastopol12 <chkalov253@gmail.com>
2026-08-04 10:56:41 +0700

    feat(convert_type): add protection for Nonetype object

--- dags/tasks/extract/utils/__init__.py
+++ dags/tasks/extract/utils/__init__.py
@@ -6,11 +6,17 @@ from .queue import ProductionQueue
 def decode_and_convert_to_records(file_format: list[str], encoded_bytes: list[str]):
     storage = []
     for file_fmt, encoded_data in zip(file_format, encoded_bytes):
-        raw_bytes = base64.b64decode(encoded_data)
-
-        storage.append({"format": file_fmt, "raw_bytes": raw_bytes})
-
+        if file_fmt and encoded_data:
+            raw_bytes = base64.b64decode(encoded_data)
+            storage.append({"format": file_fmt, "raw_bytes": raw_bytes})
+            
     return storage
 
 
-__all__ = ["ExtractorConfig", "ResponseBaseWait", "ProductionQueue", "Extractor", "decode_and_convert_to_records"]
+__all__ = [
+    "ExtractorConfig",
+    "ResponseBaseWait",
+    "ProductionQueue",
+    "Extractor",
+    "decode_and_convert_to_records",
+]
