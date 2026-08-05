90feae94cea3368cdbaf73ebf7ad8ad521181fa3
Sevastopol12 <chkalov253@gmail.com>
2026-08-05 13:51:51 +0700

    refactor: seperate ETL logic from tasks

--- dags/tasks/extract/__init__.py
+++ dags/tasks/extract/__init__.py
@@ -1,6 +1,4 @@
 import logging
-import json
-import base64
 
 from asyncio import create_task, gather
 from pydantic import BaseModel
@@ -10,16 +8,8 @@ from .utils import (
     ProductionQueue,
     ExtractorConfig,
     Extractor,
-    decode_and_convert_to_records,
 )
-from dags.storage import (
-    EventItem,
-    DataChunkManager,
-    StatusType,
-    RDBCredential,
-    CloudStorageConnection,
-    S3Credential,
-)
+from dags.storage import EventItem
 
 
 logger = logging.getLogger(__name__)
@@ -73,67 +63,4 @@ async def extract(tasks: list[dict], models: list[str]):
         return 0
 
 
-async def run_etl():
-    models = ["gemma-4-31b-it", "gemma-4-26b-a4b-it"]
-
-    data_chunk_manager = DataChunkManager.establish_connection(
-        credential=RDBCredential()
-    )
-    staging_database_connection = CloudStorageConnection.establish_connection(
-        credential=S3Credential(bucket_type="STG", bucket_name="staging"),
-        supported_format=".parquet",
-    )
-
-    all_chunk_status = await data_chunk_manager.all_chunk_status()
-
-    async with staging_database_connection:
-        for chunk_status in all_chunk_status:
-            logger.info(f"Processing: {chunk_status}")
-
-            if chunk_status["status"] != StatusType.PENDING:
-                continue
-            data_chunk = await staging_database_connection.read_parquet_file(
-                chunk_id=chunk_status["chunk_id"]
-            )
-
-            tasks = decode_and_convert_to_records(
-                file_format=data_chunk.get("format"),
-                encoded_bytes=data_chunk.get("encoded_bytes"),
-            )
-
-            logger.info("Data converted to ideal format. Extracting content...")
-
-            results = await extract(tasks=tasks, models=models)
-
-            batch_status: StatusType = results["status"]
-            succeed: list[BaseModel] = results["succeed"]
-            failed: list[dict] = results["failed"]
-
-            logger.info("Storing results.")
-
-            with open("tester/succeed.jsonl", "a", encoding="utf-8") as file:
-                data = "\n".join(
-                    item.model_dump_json() for item in succeed if item.is_event
-                )
-                file.write(data)
-
-            with open("tester/failed.jsonl", "a", encoding="utf-8") as file:
-                for obj in failed:
-                    if isinstance(obj["raw_bytes"], bytes):
-                        obj["raw_bytes"] = base64.b64encode(obj["raw_bytes"]).decode(
-                            "utf-8"
-                        )
-                    file.write(json.dumps(obj) + "\n")
-
-            logger.info("Done.")
-
-            rowcount = await data_chunk_manager.update_chunk_status(
-                chunk_id=chunk_status["chunk_id"], status=batch_status
-            )
-            logger.info(f"Chunk marked as done: {'True' if rowcount else 'False'}")
-
-    logger.info("Extract complete.")
-    return 1
-
-
-__all__ = ["run_etl", "extract"]
+__all__ = ["extract"]
